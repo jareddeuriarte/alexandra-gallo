74ca36c0ec9e496872c98104dcc452c7ea63561d
Jared de Uriarte <jared.deuri@gmail.com>
2021-08-03 01:29:45 -0400

    hero sketched out

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Bio from "./components/Bio/index"
 import Projects from "./components/Projects/index"
 import Header from "./components/Header/index"
+import Hero from "./components/Hero/index"
 
 
 
@@ -13,6 +14,7 @@ function App() {
 
     <div>
       <Header></Header>
+      <Hero></Hero>
       <Bio></Bio>
       <Projects></Projects>
 
--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,11 +1,19 @@
 import React from "react";
 import "./hero.css";
 
+// Give this component props to make it resuable and dry
+// and create "images.json" to pass through the src attribute
+//  and import json to App.js
 
 function Hero() {
   return (
-    <div>
-        <img></img>
+    <div id='hero-container'>
+        
+        <img alt='image' src='/images/alex-on-bridge.jpg'></img>
+        <img alt='image' src='/images/alex-and-level.jpg'></img>
+        <img alt='image' src='/images/alex-on-rooftop.jpg'></img>
+
+
     </div>
   );
 }
