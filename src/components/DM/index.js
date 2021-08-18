2d8afb37514c006487561e79d7847cff5d3931b3
Jared de Uriarte <jared.deuri@gmail.com>
2021-08-18 15:24:21 -0400

    pdf links added to dm component

--- src/components/DM/index.js
+++ src/components/DM/index.js
@@ -6,6 +6,12 @@ import figures from '../../assets/dm/figures.png'
 import romper from '../../assets/dm/romper.png'
 import tierra from '../../assets/dm/tierra.png'
 import united from '../../assets/dm/united.png'
+import tierraPDF from '../../assets/dm/tierra.pdf'
+import romperPDF from '../../assets/dm/romper.pdf'
+import unitedPDF from '../../assets/dm/united.pdf'
+
+
+
 
 
 function DM() {
@@ -15,17 +21,28 @@ function DM() {
 
 
 
-      <div id='dm-flex-container'>
-        <div id='dm-grid'>
-          <img className='dm-photo' alt='placeholder' src={donatella}></img>
-          <img className='dm-photo' alt='placeholder' src={tierra}></img>
-          <img className='dm-photo' alt='placeholder' src={figure}></img>
-          <img className='dm-photo' alt='placeholder' src={united}></img>
-          <img className='dm-photo' alt='placeholder' src={figures}></img>
-          <img className='dm-photo' alt='placeholder' src={romper}></img>
+    <div id='dm-flex-container'>
+      <div id='dm-grid'>
+        <img className='dm-photo' alt='placeholder' src={donatella}></img>
 
-        </div>
+        <a className='pdf' target="_blank" rel="noopener noreferrer" href={tierraPDF}>
+          <img className='dm-photo' alt='placeholder' src={tierra}></img>
+        </a>
+
+        <img className='dm-photo' alt='placeholder' src={figure}></img>
+
+        <a className='pdf' target="_blank" rel="noopener noreferrer" href={unitedPDF}>
+          <img className='dm-photo' alt='placeholder' src={united}></img>
+        </a>
+
+        <img className='dm-photo' alt='placeholder' src={figures}></img>
+
+        <a className='pdf' target="_blank" rel="noopener noreferrer" href={romperPDF}>
+          <img className='dm-photo' alt='placeholder' src={romper}></img>
+        </a>
+
       </div>
+    </div>
 
 
 
